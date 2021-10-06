ffae3d8ed3a06d0d57f5055c74af92166402dbf9
tabskey1@gmail.com <tabskey1@gmail.com>
2021-10-06 01:57:17 -0500

    Main page and create page

---
+++ RoadrunnerStay1/RoadrunnerStay/src/application/CreateAccountController.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateAccountController {
+	
+	
+	
+	@FXML private TextField userNameInput;
+	//@FXML private TextField passwordInput;
+	@FXML private PasswordField passwordInput;
+	@FXML private TextField nameInput;
+	@FXML private TextField emailInput;
+	@FXML private TextField companyNameInput;
+	@FXML private TextField positionInput;
+	
+	@FXML 
+	public void changeScreenHome(ActionEvent event) throws IOException {
+		MainController home = new MainController();
+		home.changeScreenonBack(event);
+	}
+	
+	@FXML 
+	public void changeScreenCreateAccount(ActionEvent event) throws IOException {
+		MainController home = new MainController();
+		home.changeScreenonBack(event);
+	}
+	
+	@FXML 
+	public void createAccount(ActionEvent event) throws IOException {
+		// get user input store in string
+		String username = userNameInput.getText();
+		//String passWord = passwordInput.getText();
+		String passWord = passwordInput.getText();
+		String name = nameInput.getText();
+		String email = emailInput.getText();
+		String companyName = companyNameInput.getText();
+		String position = positionInput.getText();
+		
+		// print user input
+		System.out.println("username is: "+ username + "\n");
+		System.out.println("passWord is: "+ passWord + "\n");
+		System.out.println("name is: "+ name + "\n");
+		System.out.println("email is: "+ email + "\n");
+		System.out.println("Company Name is: "+ companyName + "\n");
+		System.out.println("position is: "+ position + "\n");
+		
+		// clear textfields
+		userNameInput.clear();
+		passwordInput.clear();
+		nameInput.clear();
+		emailInput.clear();
+		companyNameInput.clear();
+		positionInput.clear();
+	}
+}
