01d6d82eef0f83ebaf2ce790cdf0fcdedbce2223
tabskey1@gmail.com <tabskey1@gmail.com>
2021-10-22 11:36:41 -0500

    added variable to see if checkbox was selected

--- RoadrunnerStay1/src/application/MainController.java
+++ RoadrunnerStay1/src/application/MainController.java
@@ -15,6 +15,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -22,13 +23,15 @@ import javafx.util.Duration;
 
 public class MainController  implements Initializable{
 	
-	@FXML
-	private Button start, back;
+	@FXML private Button start, back;
+	@FXML ImageView imageView, imageView2, imageView3;
+	@FXML CheckBox pool, gym, spa, businessOffice;
 	
-	@FXML ImageView imageView; @FXML ImageView imageView2; @FXML ImageView imageView3;
+	boolean poolSelected = false, gymSelected = false, spaSelected = false, businessOfficeSelected = false; 
 	int count = 0;
 	int count2 = 1;
 	int count3 = 2;
+	
 	
 	public void slideshow() {
 		ArrayList<Image> images = new ArrayList<Image>();
@@ -52,12 +55,33 @@ public class MainController  implements Initializable{
 		}));
 		timeline.setCycleCount(Timeline.INDEFINITE);
 		timeline.play();
-		
 	}
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources ) {
 		slideshow();
+	}
+	public void checkboxSelected() {
+		if(pool.isSelected()) {
+			poolSelected = true;
+		}
+		if(gym.isSelected()) {
+			gymSelected = true;
+		} 
+		if(spa.isSelected()) {
+			spaSelected = true;
+		} 
+		if(businessOffice.isSelected()) {
+			businessOfficeSelected = true;
+		}
+		/* send weather checkbox is selected to database*/	
+	}
+	
+	@FXML 
+	public void changeScreenResult(ActionEvent event) throws IOException {
+		checkboxSelected();
+		SwitchScenesController change = new SwitchScenesController();
+		change.changeScreenResult(event);
 	}
 	
 	@FXML 
@@ -77,10 +101,6 @@ public class MainController  implements Initializable{
 		SwitchScenesController change = new SwitchScenesController();
 		change.changeScreenProfile(event);
 	}
-	@FXML 
-	public void changeScreenResult(ActionEvent event) throws IOException {
-		SwitchScenesController change = new SwitchScenesController();
-		change.changeScreenResult(event);
-	}
+	
 
 }
